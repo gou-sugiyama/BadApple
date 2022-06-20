e9970100073e92a2893febb88d89b1be805ff739
miyarasyuma <107020925+miyarasyuma@users.noreply.github.com>
2022-06-20 12:52:53 +0900

    ブランチの作成

---
+++ GettingOverApple/GettingOverApple/key.h
@@ -0,0 +1,9 @@
+#pragma once
+class player {
+public:
+	void PlayerControl();
+	void GameInit(void);
+	int LoadImages();
+private:
+	int g_player;
+};
---
+++ GettingOverApple/GettingOverApple/player.cpp
@@ -0,0 +1,13 @@
+#include "DxLib.h"
+#include"key.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+void player::PlayerControl() {
+
+}
+int player::LoadImages() {
+
+}
+void player::GameInit() {
+
+}
