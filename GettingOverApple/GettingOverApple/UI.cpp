7d884bc5b2a7190bdb04c79ceb1376d57ddd2aa2
杉山豪 <gou-sugiyama-k21f@stu.kbc.ac.jp>
2022-06-20 22:53:51 +0900

    PAUSEの追加

--- GettingOverApple/GettingOverApple/UI.cpp
+++ GettingOverApple/GettingOverApple/UI.cpp
@@ -7,15 +7,23 @@ UserInterface::UserInterface() {
 	}
 
 	m_TimeLimit = 60 * 30;			//60フレーム*30秒
+	isPause = false;
 }
 
 void UserInterface::Draw() {
-	--m_TimeLimit;
+	if (isPause == false) {
+		--m_TimeLimit;
+	}
+	else {
+		SetFontSize(50);
+		DrawString(0, 240, "------PAUSE------", 0xFFFFFF);
+	}
+
 	DrawBox(500, 0, 640, 480, 0x009900, TRUE);
 	SetFontSize(30);
-	DrawString(508, 30, "制限時間" ,0x000000);					//数値調整済み
+	DrawString(508, 30, "制限時間", 0x000000);					//数値調整済み
 	SetFontSize(50);
-	DrawFormatString(545, 100, 0x000000, "%d", m_TimeLimit / 60);//数値調整済み
+	DrawFormatString(545, 100, 0x000000, "%02d", m_TimeLimit / 60);//数値調整済み
 	SetFontSize(16);
 	//DrawFormatString(510, 180, 0x000000, "取得したリンゴ");
 	/*DrawRotaGraph(523, 120, 0.3f, 0, g_Teki[0], TRUE, FALSE);
@@ -25,7 +33,7 @@ void UserInterface::Draw() {
 	DrawFormatString(510, 240, 0xFFFFFF, "%03d", m_AppleCount[0]);
 	DrawFormatString(560, 240, 0xFFFFFF, "%03d", m_AppleCount[1]);
 	DrawFormatString(610, 240, 0xFFFFFF, "%03d", m_AppleCount[2]);
-	
+
 	//デバッグ欄用数値
 	//(510*50*n,0xFFFFFF,"%03d" ,)
 
@@ -34,6 +42,10 @@ void UserInterface::Draw() {
 	DrawFormatString(555, 220, 0xFFFFFF, "%08d", g_Mileage / 10);
 	DrawFormatString(510, 240, 0x000000, "スピード");
 	DrawFormatString(555, 260, 0xFFFFFF, "%08d", g_player.speed);*/
+
+	if (GetJoypadInputState(DX_INPUT_KEY_PAD1) & PAD_INPUT_M) {		//スペースキーが押されたらPAUSE
+		isPause = !isPause;
+	}
 
 	if (m_TimeLimit <= 0) {
 		//TODO　制限時間が0になったら画面遷移
--- GettingOverApple/GettingOverApple/UI.h
+++ GettingOverApple/GettingOverApple/UI.h
@@ -4,8 +4,12 @@ class UserInterface
 private:
 	int m_AppleCount[4];
 	int m_TimeLimit;
+	bool isPause;
 public:
 	UserInterface();
 	void Draw(void);
+	bool getisPause() const {
+		return isPause;
+	}
 };
 
