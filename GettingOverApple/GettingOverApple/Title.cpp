db410f586c2c5f540880c482a59f08353882ad34
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2022-07-05 15:08:27 +0900

    メニューカーソル動いた

--- GettingOverApple/GettingOverApple/Title.cpp
+++ GettingOverApple/GettingOverApple/Title.cpp
@@ -4,14 +4,6 @@
 #include"Controller.h"
 #include"Game.h"
 
-enum class EMenu {
-	E_START = 0
-	,E_RANKING
-	,E_HELP
-	,E_END
-
-};
-
 CTitle::CTitle(CController* pController) :CScene(pController) {
 	titleImage = LoadGraph("images/Title.bmp");
 	startImage = LoadGraph("images/start_m.bmp");
@@ -19,18 +11,40 @@ CTitle::CTitle(CController* pController) :CScene(pController) {
 	helpImage = LoadGraph("images/help_m.bmp");
 	endImage = LoadGraph("images/end_m.bmp");
 	menuCursor = 0;
+	inputInterval = 0;
 }
 
 CScene* CTitle::Update() {
+	keyInput = controller->control(false);
 
+	SelectMenu();
 
-	if ((controller->control(false)).Buttons[4] == true) {
+	if (keyInput.Buttons[4] == true) {
 		//ゲームシーンに移行
 		return new CGame(controller);
 	}
+	
 	return this;
+}
+
+void CTitle::SelectMenu() {
+	if (keyInput.Buttons[XINPUT_BUTTON_DPAD_DOWN] == true) {
+		if (++menuCursor > D_END)menuCursor = D_START;
+	}
+	if (keyInput.Buttons[XINPUT_BUTTON_DPAD_UP] == true) {
+		if (--menuCursor < D_START)menuCursor = D_END;
+	}
 }
 
 void CTitle::Render()const {
 	DrawGraph(0, 0, titleImage, FALSE);
+	DrawMenu();
+}
+
+void CTitle::DrawMenu()const {
+	/*DrawGraph(D_POS_X, D_POS_Y + (D_HEIGHT * (int)EMenu::D_START) , startImage, TRUE);
+	DrawGraph(D_POS_X, D_POS_Y + (D_HEIGHT * (int)EMenu::D_RANKING) , rankingImage, TRUE);
+	DrawGraph(D_POS_X, D_POS_Y + (D_HEIGHT * (int)EMenu::D_HELP) , helpImage, TRUE);
+	DrawGraph(D_POS_X, D_POS_Y + (D_HEIGHT * (int)EMenu::D_END) , endImage, TRUE);*/
+	DrawFormatString(0, 0, 0XFFFFFF, "%d", menuCursor);
 }
--- GettingOverApple/GettingOverApple/Title.h
+++ GettingOverApple/GettingOverApple/Title.h
@@ -1,25 +1,37 @@
 #pragma once
-#define D_MENU_WIDTH (195)
-#define D_MENU_HEIGHT (40)
-#define D_MENU_MARGIN (40)
+#include"define.h"
+#define D_WIDTH (195)
+#define D_HEIGHT (40)
+#define D_MARGIN (40)
+#define D_POS_X  D_SCREEN_WIDTH - D_MARGIN - D_WIDTH
+#define D_POS_Y (D_SCREEN_HEIGHT - ((D_HEIGHT) * (int)EMenu::D_MARGIN))
+#define D_START (0)
+#define D_RANKING (1)
+#define D_HELP (2)
+#define D_END (3)
+#define D_MAGNIFICATION 1.2
 
 //タイトルの管理
 class CTitle :
     public CScene
 {
 private:
+    XINPUT_STATE keyInput;
     int titleImage;
     int startImage;
     int rankingImage;
     int helpImage;
     int endImage;
     int menuCursor;
+    int inputInterval;
 
 public:
     CTitle(CController* pController);      //基底クラスの引数付きコンストラクタを呼ぶには、実装時に: <基底クラス名>(<実引数リスト>) と書く。
     ~CTitle() {}
 
     CScene* Update() override;
+    void SelectMenu();
     void Render()const override;
+    void DrawMenu()const;
 };
 
