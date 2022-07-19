3c3fcb9815a64ee08fe5fd0d4f3e4de8027daf74
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2022-07-19 14:39:10 +0900

    end完成

--- GettingOverApple/GettingOverApple/End.cpp
+++ GettingOverApple/GettingOverApple/End.cpp
@@ -7,14 +7,18 @@
 // コンストラクタ
 //--------------------------------
 CEnd::CEnd(CController* pController) :CScene(pController) {
-
+	image = LoadGraph("images/haikei.png");
 }
 
 //--------------------------------
 // 更新
 //--------------------------------
 CScene* CEnd::Update() {
-	//controller->control(false);
+	static int limit = 180;
+
+	if (limit-- < 0) {
+		return nullptr;
+	}
 	return this;
 }
 
@@ -22,5 +26,5 @@ CScene* CEnd::Update() {
 // 描画
 //--------------------------------
 void CEnd::Render()const {
-	DrawString(0, 0, "エンド画面", 0xFFFFFF);
+	DrawGraph(0, 0, image, FALSE);
 }
--- GettingOverApple/GettingOverApple/End.h
+++ GettingOverApple/GettingOverApple/End.h
@@ -4,6 +4,7 @@ class CEnd :
     public CScene
 {
 private:
+    int image;
 public:
     CEnd(CController* pController);      //基底クラスの引数付きコンストラクタを呼ぶには、実装時に: <基底クラス名>(<実引数リスト>) と書く。
     ~CEnd() {}
